06f3575068c9db8e3c646c55aa4dad4ce5390151
Paul <dev@wfi>
2023-04-13 22:56:21 -0600

    rewrite partial complete

--- UTAFramework.py
+++ UTAFramework.py
@@ -1,112 +1,129 @@
 import requests
-from geopy.geocoders import Nominatim
+from math import sin, cos, sqrt, atan2, radians
 
-class UTABase:
-    def __init__(self):                 
-        self.attributes = {}
-
-    def SetValue(self, Label, Value):
-        self.attributes[Label] = Value
-
-    def GetValue(self, Label):
-        return self.attributes[Label]
-
-class Route(UTABase):
-    def __init__(self):
-        super().__init__()
-        self.SetValue("type", "Route")
-        self.SetValue("id", None)
-        self.SetValue("shortname", None)
-        self.SetValue("longname", None)
-        self.SetValue("description", None)
-        self.SetValue("url", None)
+class Route:
+    def __init__(self, ID, number, name, type):
+        self.ID = ID
+        self.number = number
+        self.name = name
+        self.type = type
+        #self.uri = f"https://www.rideuta.com/Rider-Tools/Schedules-and-Maps/{self.number}-{self.name}"
 
     def __repr__(self):
-        return f"Route {self.GetValue('shortname')} ({self.GetValue('longname')})"
+        return f"{self.type} {self.number}: {self.name}"
     
-    def GetApi(self, Token):
-        return f"http://api.rideuta.com/utartapi/vehiclemonitor/ByRoute?route={self.GetValue('shortname')}&onwardcalls=true&usertoken={Token}&format=json"
-
-    def GetStatus(self, Token):
-        uri = Frontrunner.GetApi(AuthToken)
-        r = requests.get(uri)
-        return r
+    def GetNumber(self):
+        return self.number
     
-    def GetTrains(self, Token):
-        geolocator = Nominatim(user_agent="geoapiExercises")
-        Status = self.GetStatus(Token).json()
-        Vehicles = Status['serviceDelivery']['vehicleMonitoringDelivery']['vehicleActivity']
-        for v in Vehicles:
-            mva = v['monitoredVehicleJourney']
-            latitude =  mva['vehicleLocation']['latitude']
-            longitude = mva['vehicleLocation']['longitude']
-            location = geolocator.reverse(f"{latitude},{longitude}")
-            address = location.raw['address']
-            Building = address.get('building', '')
-            HouseNum = address.get('house_number', '')
-            Road = address.get('road', '')
-            City = address.get('city', '')
-            Zip = address.get('postcode')
-            #print(address.keys())
-            #print(address)
-            print(f"Extensions:            {mva['extensions']}")
-            print(f"Line reference:        {mva['lineRef']}")
-            print(f"Journey reference:     {mva['framedVehicleJourneyRef']}")
-            print(f"Line name:             {mva['publishedLineName']}")
-            print(f"Direction:             {mva['directionName']}")
-            print(f"Origin:                {mva['originRef']}")
-            print(f"Destination:           {mva['destinationRef']}")
-            print(f"Destination name:      {mva['destinationName']}")
-            print(f"Monitored:             {mva['monitored']}")
-            print(f"Location:              {Building}, {HouseNum} {Road}, {City} {Zip}")
-            print(f"Bearing:               {mva['bearing']}")
-            print(f"Progress Status:       {mva['progressStatus']}")
-            print(f"Course:                {mva['courseOfJourneyRef']}")
-            print(f"Vehicle Reference:     {mva['vehicleRef']}")
-            print()
-            print()
+    def GetType(self):
+        return self.type
 
-        #status['serviceDelivery']['vehicleMonitoringDelivery']['vehicleActivity'][0]['monitoredVehicleJourney'].keys()
-        #dict_keys(['extensions', 'lineRef', 'framedVehicleJourneyRef', 'publishedLineName', 'directionName', 'originRef', 'destinationRef', 'destinationName', 
-        #           'monitored', 'vehicleLocation', 'bearing', 'progressStatus', 'courseOfJourneyRef', 'vehicleRef'])
-        return
+class Vehicle:
+    def __init__(self, route, location, speed, direction, destination, bearing, status, ref):
+        self.route = route
+        self.location = location
+        self.speed = speed
+        self.direction = direction
+        self.destination = destination
+        self.bearing = bearing
+        self.status = status
+        self.ref = ref
 
-def GetFavoriteRoute(Label):
-    r = Route()
-    l = Label.lower()
-    if l ==  "frontrunner":
-        r = Route()
-        r.SetValue("id", 41065)
-        r.SetValue("shortname", "750")
-        r.SetValue("longname", "FrontRunner")
-        r.SetValue("url", "https://www.rideuta.com/Rider-Tools/Schedules-and-Maps/750-FrontRunner")
-    elif l == "834":
-        r.SetValue("id", 39024)
-        r.SetValue("shortname", "834")
-        r.SetValue("longname", "VINEYARD/RIVERWOODS/ PROVO STATION")
-        r.SetValue("url", "https://www.rideuta.com/Rider-Tools/Schedules-and-Maps/834-Riverwoods---Provo-Station")
+    def __repr__(self):
+        return f"{self.route.GetNumber()} {self.route.GetType()} #{self.ref}: {self.status} traveling {self.direction} to {self.destination} at {self.speed:.1f} mph bearing {self.bearing:.0f}, {self.DescribeLocation()}"
 
-    return r
+    def DescribeLocation(self):
+        MurrayCentral = [40.659902519348506, -111.89437902728629]
+        
+        R = 6373.0  # Approximate radius of earth in km
+        lat1 = radians(MurrayCentral[0])
+        lon1 = radians(MurrayCentral[1])
+        lat2 = radians(float(self.location[0]))
+        lon2 = radians(float(self.location[1]))
 
-def PrintStatus(StatusJson):
-    j = StatusJson
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+
+        a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        distancekm = R * c                          # distance in kilometers
+        distance = distancekm * 0.621371            # 0.621371 miles in a kilometer
+        direction = 'north' if lat1 < lat2 else 'south'
+
+        return f"{distance:.1f} miles {direction} of Murray Central"
+
+def GetMvaValue(mva, key, key2, default):
+    if key in mva:
+        if key2 in mva[key]:
+            return mva[key][key2]
+    return default
+
+def GetMvaValueFromList(mva, key, key2, default):
+    if key in mva:
+        if key2 in mva[key][0]:
+            return mva[key][0][key2]
+    return default
+
+def GetVehicleStatus(route, authtoken):
+    apiuri = f"http://api.rideuta.com/utartapi/vehiclemonitor/ByRoute?route={route.number}&onwardcalls=true&usertoken={authtoken}&format=json"
+    j = requests.get(apiuri).json()
+    statuses = j['serviceDelivery']['vehicleMonitoringDelivery']['vehicleActivity']
+    VehicleStatus = []
+    GPSDuplicates = {}
+ 
+    for s in statuses:
+        mva = s['monitoredVehicleJourney']
+        print(mva)
+        latitude =  mva['vehicleLocation']['latitude']
+        longitude = mva['vehicleLocation']['longitude']
+        location = [latitude, longitude]
+        bearing = float(mva['bearing'])
+        speed = float(GetMvaValue(mva, 'extensions', 'speed', '0.0'))
+        direction = GetMvaValueFromList(mva, 'directionName', 'value', "Unknown")
+        destination = GetMvaValueFromList(mva, 'destinationName', 'value', 'Unknown')
+        status = GetMvaValueFromList(mva, 'progressStatus', 'value', "On time")
+        ref = GetMvaValue(mva, 'vehicleRef', 'value', 'Unknown')
+        if direction != 'Unknown':
+            locationstr = str(location[0]) + str(location[0])
+            if locationstr not in GPSDuplicates:
+                v = Vehicle(route, location, speed, direction, destination, bearing, status, ref)
+                VehicleStatus.append(v)
+                GPSDuplicates[locationstr] = True
+    return  sorted(VehicleStatus, key=lambda x: x.direction, reverse=False)
+
+FavoriteRoutes = {
+    750: Route(41065, 750, "Frontrunner", "Train"),
+    834: Route(39024, 834, "VINEYARD/RIVERWOODS/ PROVO STATION", "Bus")
+}
 
 # Load in a couple of routes
 #
-r834 = GetFavoriteRoute("834")
-Frontrunner = GetFavoriteRoute("Frontrunner")
-print(r834)
+Route834 = FavoriteRoutes[834]
+Frontrunner = FavoriteRoutes[750]
+print()
+print(Route834)
 print(Frontrunner)
+print()
 
 # Load in the token
 #
 with open('token.cfg') as f:
     lines = f.readlines()
 AuthToken = lines[0].strip()
-print(AuthToken)
+# print(AuthToken)
 
 # Get the status for the frontrunner
 #
-Frontrunner.GetTrains(AuthToken)
+VehicleStatus = GetVehicleStatus(Frontrunner, AuthToken)
+print(f"{len(VehicleStatus)} active Frontrunner trains:")
+print(f"----------------")
+for Train in VehicleStatus:
+    print(Train)
+print()
+    
+# status['serviceDelivery']['vehicleMonitoringDelivery']['vehicleActivity'][0]['monitoredVehicleJourney'].keys()
+# dict_keys(['extensions', 'lineRef', 'framedVehicleJourneyRef', 'publishedLineName', 'directionName', 'originRef', 'destinationRef', 'destinationName', 
+#           'monitored', 'vehicleLocation', 'bearing', 'progressStatus', 'courseOfJourneyRef', 'vehicleRef'])
 
 
